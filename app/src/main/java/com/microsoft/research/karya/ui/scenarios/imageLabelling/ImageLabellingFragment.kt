cb1147f98a563ba88b1d89d9655b7fb2052f6920
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2022-10-06 19:34:30 +0530

    migrated ImageLabellingFragment to viewbinding

--- app/src/main/java/com/microsoft/research/karya/ui/scenarios/imageLabelling/ImageLabellingFragment.kt
+++ app/src/main/java/com/microsoft/research/karya/ui/scenarios/imageLabelling/ImageLabellingFragment.kt
@@ -7,21 +7,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.microsoft.research.karya.R
+import com.microsoft.research.karya.databinding.ItemFloatLabelBinding
+import com.microsoft.research.karya.databinding.MicrotaskImageLabellingBinding
 import com.microsoft.research.karya.ui.scenarios.common.BaseMTRendererFragment
 import com.microsoft.research.karya.utils.extensions.observe
+import com.microsoft.research.karya.utils.extensions.viewBinding
 import com.microsoft.research.karya.utils.extensions.viewLifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.item_float_label.view.*
-import kotlinx.android.synthetic.main.microtask_image_labelling.*
 
 @AndroidEntryPoint
 class ImageLabellingFragment : BaseMTRendererFragment(R.layout.microtask_image_labelling) {
     override val viewModel: ImageLabellingViewModel by viewModels()
     private val args: ImageLabellingFragmentArgs by navArgs()
+
+    private val binding by viewBinding(MicrotaskImageLabellingBinding::bind)
 
     private val labelMap: MutableMap<String, View> = mutableMapOf()
 
@@ -42,28 +46,30 @@ class ImageLabellingFragment : BaseMTRendererFragment(R.layout.microtask_image_l
             } catch (e: Exception) {
                 getString(R.string.image_labelling_instruction)
             }
-        instructionTv.text = instruction
+        with(binding) {
+            instructionTv.text = instruction
 
-        // Set next button click handler
-        nextBtn.setOnClickListener { handleNextClick() }
+            // Set next button click handler
+            nextBtn.setOnClickListener { handleNextClick() }
 
-        // Set up label views
-        val labels =
-            try {
-                viewModel.task.params.asJsonObject.get("labels").asJsonArray.map { it.asString }
-            } catch (e: Exception) {
-                arrayListOf()
+            // Set up label views
+            val labels =
+                try {
+                    viewModel.task.params.asJsonObject.get("labels").asJsonArray.map { it.asString }
+                } catch (e: Exception) {
+                    arrayListOf()
+                }
+
+            labels.forEach { value ->
+                ItemFloatLabelBinding.inflate(layoutInflater).apply {
+                    label.text = value
+
+                    root.setOnClickListener { viewModel.flipState(value) }
+                    labelsLayout.addView(root)
+
+                    labelMap[value] = root
+                }
             }
-
-        labels.forEach {
-            val label = it
-            val labelView = layoutInflater.inflate(R.layout.item_float_label, null)
-            labelView.label.text = label
-
-            labelView.setOnClickListener { viewModel.flipState(label) }
-            labelsLayout.addView(labelView)
-
-            labelMap[label] = labelView
         }
     }
 
@@ -75,9 +81,9 @@ class ImageLabellingFragment : BaseMTRendererFragment(R.layout.microtask_image_l
         viewModel.imageFilePath.observe(viewLifecycleOwner.lifecycle, viewLifecycleScope) { path ->
             if (path.isNotEmpty()) {
                 val image: Bitmap = BitmapFactory.decodeFile(path)
-                sourceImageIv.setImageBitmap(image)
+                binding.sourceImageIv.setImageBitmap(image)
             } else {
-                sourceImageIv.setImageDrawable(null)
+                binding.sourceImageIv.setImageDrawable(null)
             }
         }
 
@@ -86,7 +92,7 @@ class ImageLabellingFragment : BaseMTRendererFragment(R.layout.microtask_image_l
             labelMap.forEach { (s, view) ->
                 val state = labelState[s] ?: false
                 val color = if (state) R.color.c_light_green else R.color.c_light_grey
-                view.label_card.background.setTint(ContextCompat.getColor(requireContext(), color))
+                view.findViewById<CardView>(R.id.label_card).background.setTint(ContextCompat.getColor(requireContext(), color))
             }
         }
     }
