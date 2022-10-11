c4df56ff79e63a89b7d26a7cde0c5b63141cfd59
Divyansh <divyansh@pop-os.localdomain>
2022-10-12 01:17:36 +0530

    removed kotlin synthetics from ImageListAdapter

--- app/src/main/java/com/microsoft/research/karya/ui/scenarios/imageData/ImageListAdapter.kt
+++ app/src/main/java/com/microsoft/research/karya/ui/scenarios/imageData/ImageListAdapter.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.microsoft.research.karya.R
 import com.microsoft.research.karya.utils.ImageUtils.bitmapFromFile
 import com.microsoft.research.karya.utils.extensions.invisible
 import com.microsoft.research.karya.utils.extensions.visible
-import kotlinx.android.synthetic.main.microtask_image_data_grid_view_item.view.*
 
 class ImageListAdapter(
     private var context: Context,
@@ -31,19 +32,21 @@ class ImageListAdapter(
 
     override fun getView(index: Int, p1: View?, p2: ViewGroup?): View {
         val view = p1 ?: LayoutInflater.from(context).inflate(R.layout.microtask_image_data_grid_view_item, p2, false)
+        val altText = view.findViewById<TextView>(R.id.altText)
+        val image = view.findViewById<ImageView>(R.id.image)
 
         // Extract info
         val path = images[index]
         val text = if (index == 0) "Front" else "P$index"
 
         if (path == "") {
-            view.altText.text = text
-            view.altText.visible()
-            view.image.invisible()
+            altText.text = text
+            altText.visible()
+            image.invisible()
         } else {
-            view.image.setImageBitmap(bitmapFromFile(path))
-            view.image.visible()
-            view.altText.invisible()
+            image.setImageBitmap(bitmapFromFile(path))
+            image.visible()
+            altText.invisible()
         }
 
         view.setOnClickListener { imageClickListener(index) }
